99e3b0c8d49ca5ab9d0094fb3b4c8db8331279d8
Max Dubrinsky <msdubrin@mtu.edu>
2021-04-29 18:46:12 -0400

    make it all work

--- api/name.ts
+++ api/name.ts
@@ -1,16 +1,20 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import fetch from "node-fetch";
 
-async function makeWordsCall(part: string) {
-  const res = await fetch(
-    `https://wordsapiv1.p.mashape.com/words/?partOfSpeech=${part}&random=true`,
-    {
-      headers: {
-        "X-Mashape-Key": process.env.WORDS_API_KEY,
-        Accept: "application/json",
-      },
-    }
-  );
+async function makeWordsCall(part: string, startsWith: string) {
+  const url = new URL("/words/", "https://wordsapiv1.p.rapidapi.com");
+
+  url.searchParams.append("partOfSpeech", part);
+  url.searchParams.append("letterPattern", `^${startsWith}\\w+$`);
+  url.searchParams.append("random", "true");
+
+  const res = await fetch(url, {
+    headers: {
+      "x-rapidapi-key": process.env.WORDS_API_KEY,
+      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+      Accept: "application/json",
+    },
+  });
 
   const data: { word: string } = await res.json();
 
@@ -19,9 +23,9 @@ async function makeWordsCall(part: string) {
 
 async function name(req: VercelRequest, res: VercelResponse) {
   const [adverb, verb, noun] = await Promise.all([
-    makeWordsCall("adverb"),
-    makeWordsCall("verb"),
-    makeWordsCall("noun"),
+    makeWordsCall("adverb", "f"),
+    makeWordsCall("verb", "k"),
+    makeWordsCall("noun", "j"),
   ]);
 
   res.json({ adverb, verb, noun });
--- src/App.svelte
+++ src/App.svelte
@@ -7,9 +7,52 @@
 </script>
 
 <main>
-  {#await getName() then name}
+  {#await getName()}
+    <div />
+  {:then name}
     <h1>{name.adverb} {name.verb} {name.noun}</h1>
   {:catch}
     <h1>failure kraken joined</h1>
   {/await}
+  <footer>
+    <a target="_blank" href="https://github.com/maxdubrinsky"
+      ><i class="ri-github-fill" /></a
+    >
+  </footer>
 </main>
+
+<style>
+  main {
+    display: grid;
+    align-items: center;
+    grid-template-rows: 1fr auto;
+    text-align: center;
+    font-family: "Oxygen", sans-serif;
+    min-height: 100vh;
+    color: var(--dark-color);
+    background-color: var(--light-color);
+  }
+
+  @media (prefers-color-scheme: dark) {
+    main {
+      background-color: var(--dark-color);
+      color: var(--light-color);
+    }
+  }
+
+  h1 {
+    font-size: 45px;
+    font-weight: 300;
+    text-transform: capitalize;
+  }
+  footer {
+    display: flex;
+    justify-content: center;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    font-size: 40px;
+  }
+</style>
